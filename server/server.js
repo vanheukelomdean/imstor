cfce2bf7240d2ddf2c3158bc8b13ed9491e0c159
Dean Van Heukelom <vanheukelomdean@gmail.com>
2021-01-17 21:05:16 -0500

    changing node env to prod

--- server/server.js
+++ server/server.js
@@ -29,7 +29,7 @@ app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
 
-console.log(process.env.Node_ENV);
+console.log(process.env);
 if (process.env.Node_ENV === 'production') {
     const serve_path = path.resolve(__dirname, '..', 'build', 'index.html');
     console.log("Serving " + serve_path);
