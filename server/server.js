6a7673d21435c86278849edb4df92cb9e7a76fa3
Dean Van Heukelom <vanheukelomdean@gmail.com>
2021-01-17 19:58:46 -0500

    changes to package.json

--- server/server.js
+++ server/server.js
@@ -28,7 +28,15 @@ let app = express();
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(bodyParser.json());
-app.use(express.static(path.join(__dirname, 'client/build')));
+
+
+if (process.env.Node_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('/*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
 
 app.use('/api/images', images);
 app.use('/api/users', users);
