af23b4c9f1044bea9e778c5f151468f5eb27ad52
Dean Van Heukelom <vanheukelomdean@gmail.com>
2021-01-18 09:37:15 -0500

    pushing final to remote

--- server/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    region: 'us-east-1',
-    accessKeyId: 'AKIA2CEGHLFXGTZ7EOT3',
-    secretAccessKey: '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',
-    GEN_USER: 'imstor_general',
-    GEN_PWD: 'imgrepo',
-    DB_NAME: 'imstor',
-}
